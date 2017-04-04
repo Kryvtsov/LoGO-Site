db6911af8d0736df973939f16493b21d2b100222
Vladymyr <kryvtsov.v@gmail.com>
2017-04-04 08:43:15 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function cat1() {
+    document.getElementById('pictures_cat1').style.display = '';
+    document.getElementById('pictures_cat2').style.display = 'none';
+}
+function cat2() {
+    document.getElementById('pictures_cat2').style.display = '';
+    document.getElementById('pictures_cat1').style.display = 'none';
+}
